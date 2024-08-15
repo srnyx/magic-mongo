78dde0b32654adb6e2b7ee41976cd7699674ba66
srnyx <25808801+srnyx@users.noreply.github.com>
2024-08-14 20:33:02 -0400

    Update dependencies
    Gradle Galaxy: 1.1.2 -> 1.2.3
    Shadow: 8.1.1 -> 8.3.0
    MongoDB Driver Sync: 5.0.0 -> 5.1.3
    Gradle: 8.3 -> 8.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,22 @@
 import xyz.srnyx.gradlegalaxy.data.pom.DeveloperData
 import xyz.srnyx.gradlegalaxy.data.pom.LicenseData
+import xyz.srnyx.gradlegalaxy.enums.Repository
+import xyz.srnyx.gradlegalaxy.enums.repository
 import xyz.srnyx.gradlegalaxy.utility.setupJava
 import xyz.srnyx.gradlegalaxy.utility.setupPublishing
 
 
 plugins {
     java
-    id("xyz.srnyx.gradle-galaxy") version "1.1.2"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("xyz.srnyx.gradle-galaxy") version "1.2.3"
+    id("com.gradleup.shadow") version "8.3.0"
 }
 
 setupJava("xyz.srnyx", "1.2.2", "Common framework for srnyx's MongoDB management", JavaVersion.VERSION_1_8)
 
-repositories.mavenCentral()
+repository(Repository.MAVEN_CENTRAL)
 dependencies {
-    implementation("org.mongodb", "mongodb-driver-sync", "5.0.0")
+    implementation("org.mongodb", "mongodb-driver-sync", "5.1.3")
     compileOnly("org.jetbrains" , "annotations", "24.1.0")
 }
 
