6efd2020d9afcb244ddb33832cbd2c824b792b4a
srnyx <25808801+srnyx@users.noreply.github.com>
2024-08-29 17:16:51 -0400

    Merge `MultiMongo` and `MagicMongo`

--- src/main/java/xyz/srnyx/magicmongo/MagicMongo.java
+++ src/main/java/xyz/srnyx/magicmongo/MagicMongo.java
@@ -13,28 +13,41 @@ import org.bson.codecs.pojo.PojoCodecProvider;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 /**
  * A class for managing Mongo using {@link MagicDatabase MagicDatabases} and {@link MagicCollection MagicCollections}
  *
  * @see SingleMongo
- * @see MultiMongo
  */
 public class MagicMongo {
-    @NotNull protected final ConnectionString connection;
-    @NotNull public final MongoClient client;
-    @Nullable private final CodecRegistry codecRegistry;
-
     /**
-     * Creates a new {@link MagicMongo} instance
-     *
-     * @param   connectionUrl   the connection URL for the MongoDB database
-     * @param   codecRegistry   the {@link CodecRegistry} to use for the MongoDB database
+     * The {@link ConnectionString} for the MongoDB connection
      */
+    @NotNull public final ConnectionString connection;
+    /**
+     * The {@link MongoClient} instance for the MongoDB connection
+     */
+    @NotNull public final MongoClient client;
+    /**
+     * The {@link CodecRegistry} to use when creating {@link MagicDatabase MagicDatabases}
+     */
+    @Nullable private final CodecRegistry codecRegistry;
+    /**
+     * The {@link MagicDatabase MagicDatabases} that have been loaded
+     */
+    @NotNull public final Map<String, MagicDatabase> databases = new HashMap<>();
+
     public MagicMongo(@NotNull String connectionUrl, @Nullable CodecRegistry codecRegistry) {
-        this.connection = new ConnectionString(connectionUrl);
+        connection = new ConnectionString(connectionUrl);
         client = MongoClients.create(connection);
         this.codecRegistry = codecRegistry;
+
+        // Load database if specified
+        final String databaseName = connection.getDatabase();
+        if (databaseName != null) loadMagicDatabase(databaseName);
     }
 
     /**
@@ -60,5 +73,61 @@ public class MagicMongo {
         final MongoDatabase database = client.getDatabase(name);
         if (codecRegistry != null) database.withCodecRegistry(codecRegistry);
         return new MagicDatabase(database);
+    }
+
+    /**
+     * Constructs a new {@link MagicDatabase} with the specified name and loads it
+     *
+     * @param   name    the name of the database to create
+     *
+     * @return          the new {@link MagicDatabase} instance
+     */
+    @NotNull
+    public MagicDatabase loadMagicDatabase(@NotNull String name) {
+        final MagicDatabase database = newMagicDatabase(name);
+        databases.put(name, database);
+        return database;
+    }
+
+    /**
+     * Constructs new {@link MagicDatabase MagicDatabases} with the specified names and loads them
+     *
+     * @param   toLoad  the names of the databases to create
+     *
+     * @return          this {@link MagicMongo} instance
+     */
+    @NotNull
+    public MagicMongo loadMagicDatabases(@NotNull Iterable<String> toLoad) {
+        toLoad.forEach(this::loadMagicDatabase);
+        return this;
+    }
+
+    /**
+     * Constructs new {@link MagicDatabase MagicDatabases} with the specified names and loads them
+     *
+     * @param   toLoad  the names of the databases to create
+     *
+     * @return          this {@link MagicMongo} instance
+     */
+    @NotNull
+    public MagicMongo loadMagicDatabases(@NotNull String... toLoad) {
+        for (final String name : toLoad) loadMagicDatabase(name);
+        return this;
+    }
+
+    /**
+     * Gets a {@link MagicDatabase} by name
+     *
+     * @param   name                        the name of the database to get
+     *
+     * @return                              the {@link MagicDatabase} instance
+     *
+     * @throws  IllegalArgumentException    if no {@link MagicDatabase} is found with the specified name
+     */
+    @NotNull
+    public MagicDatabase getMagicDatabase(@NotNull String name) {
+        final MagicDatabase collection = databases.get(name);
+        if (collection == null) throw new IllegalArgumentException("No MagicDatabase found with name " + name);
+        return collection;
     }
 }
--- src/main/java/xyz/srnyx/magicmongo/SingleMongo.java
+++ src/main/java/xyz/srnyx/magicmongo/SingleMongo.java
@@ -5,22 +5,20 @@ import org.bson.codecs.configuration.CodecRegistry;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Objects;
+
 
 /**
  * A {@link MagicMongo} instance with a single {@link MagicDatabase} that's specified in the connection URL
  */
 public class SingleMongo extends MagicMongo {
     /**
-     * The {@link MagicDatabase} instance for the single database
+     * The {@link MagicDatabase} that's specified in the connection URL
      */
     @NotNull public final MagicDatabase database;
 
     public SingleMongo(@NotNull String connectionUrl, @Nullable CodecRegistry codecRegistry) {
         super(connectionUrl, codecRegistry);
-
-        // Load database
-        final String databaseName = connection.getDatabase();
-        if (databaseName == null) throw new IllegalArgumentException("No database name found in connection URL: " + connectionUrl);
-        database = newMagicDatabase(databaseName);
+        database = Objects.requireNonNull(databases.get(connection.getDatabase()), "Database name not specified in connection URL: " + connectionUrl);
     }
 }
