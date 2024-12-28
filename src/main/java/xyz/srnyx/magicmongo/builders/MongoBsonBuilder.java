ef0c2370bdc2776e4a454a2254b0fbf8621d5c62
srnyx <25808801+srnyx@users.noreply.github.com>
2024-12-28 18:57:39 -0500

    New `MongoBsonBuilder` methods
    - `#isNull()`
    - `#get()`

--- src/main/java/xyz/srnyx/magicmongo/builders/MongoBsonBuilder.java
+++ src/main/java/xyz/srnyx/magicmongo/builders/MongoBsonBuilder.java
@@ -5,6 +5,8 @@ import org.bson.conversions.Bson;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Optional;
+
 
 /**
  * A simple builder for {@link Bson MongoDB Bson} objects
@@ -56,11 +58,31 @@ public abstract class MongoBsonBuilder<T extends MongoBsonBuilder<T>> {
     public abstract MongoBsonBuilder<T> clone();
 
     /**
+     * Checks if the {@link #bson} is null
+     *
+     * @return  whether the {@link #bson} is null
+     */
+    public boolean isNull() {
+        return bson == null;
+    }
+
+    /**
+     * Gets the {@link Bson} object
+     * <br><b>You should use {@link #build()} instead!</b>
+     *
+     * @return  the {@link Bson} object
+     *
+     * @see     #build()
+     */
+    @NotNull
+    public Optional<Bson> get() {
+        return Optional.ofNullable(bson);
+    }
+
+    /**
      * Builds the {@link Bson} object
      *
-     * @return                          the built {@link Bson} object
-     *
-     * @throws  IllegalStateException   if the {@link #bson} is null
+     * @return  the built {@link Bson} object or the {@link #ifNull() default} if the {@link #bson} is null
      */
     @NotNull
     public Bson build() {
