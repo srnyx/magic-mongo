8904c4a29019a3c9ffa9299dc68342e7bb81f08b
srnyx <25808801+srnyx@users.noreply.github.com>
2026-04-07 20:35:38 -0400

    Use new publishing system

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,25 @@
+import xyz.srnyx.gradlegalaxy.data.config.JavaSetupConfig
+import xyz.srnyx.gradlegalaxy.data.config.publishingSimpleConfig
 import xyz.srnyx.gradlegalaxy.data.pom.DeveloperData
 import xyz.srnyx.gradlegalaxy.data.pom.LicenseData
 import xyz.srnyx.gradlegalaxy.enums.Repository
 import xyz.srnyx.gradlegalaxy.enums.repository
 import xyz.srnyx.gradlegalaxy.utility.setupJava
-import xyz.srnyx.gradlegalaxy.utility.setupPublishing
+import xyz.srnyx.gradlegalaxy.utility.setupPublishingEnv
 
 
 plugins {
     java
-    id("xyz.srnyx.gradle-galaxy") version "1.3.3"
-    id("com.gradleup.shadow") version "8.3.5"
+    `java-library`
+    id("xyz.srnyx.gradle-galaxy") version "2.1.0"
+    id("com.gradleup.shadow") version "8.3.9"
 }
 
-setupJava("xyz.srnyx", "2.0.0", "Common framework for srnyx's MongoDB management", JavaVersion.VERSION_1_8)
+setupJava(JavaSetupConfig(
+    group = "xyz.srnyx",
+    version = "2.0.0",
+    description = "Common framework for srnyx's MongoDB management",
+    javaVersion = JavaVersion.VERSION_1_8))
 
 repository(Repository.MAVEN_CENTRAL)
 dependencies {
@@ -20,8 +27,8 @@ dependencies {
     compileOnly("org.jetbrains:annotations:26.1.0")
 }
 
-setupPublishing(
+setupPublishingEnv(publishingSimpleConfig(
     artifactId = "magic-mongo",
-    url = "https://magic-mongo.srnyx.com",
+    url = "https://github.com/srnyx/magic-mongo",
     licenses = listOf(LicenseData.MIT),
-    developers = listOf(DeveloperData.srnyx))
+    developers = listOf(DeveloperData.srnyx)))
